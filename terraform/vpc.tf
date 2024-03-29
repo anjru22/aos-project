2bfb85d2b391c93747b32b4243d9542aa76dc65f
Andrew Chu <andrewchu2298@gmail.com>
2024-03-29 14:32:32 -0700

    3-29-2024

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,23 +1,23 @@
 # VPC
-resource "aws_vpc" "dev-vpc" {
+resource "aws_vpc" "dynamic-app-vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   tags = {
-    Name = "dev vpc"
+    Name = "dynamic app vpc"
   }
 }
 
 # Internet Gateway
-resource "aws_internet_gateway" "dev-igw" {
-  vpc_id = aws_vpc.dev-vpc.id
+resource "aws_internet_gateway" "dynamic-app-vpc-igw" {
+  vpc_id = aws_vpc.dynamic-app-vpc.id
   tags = {
-    Name = "dev vpc igw"
+    Name = "dynamic app vpc igw"
   }
 }
 
 # Public Subnet AZ 1
 resource "aws_subnet" "pub-subnet-az1" {
-  vpc_id     = aws_vpc.dev-vpc.id
+  vpc_id     = aws_vpc.dynamic-app-vpc.id
   cidr_block = "10.0.0.0/24"
   availability_zone = "us-west-1a"
   map_public_ip_on_launch = true
@@ -29,7 +29,7 @@ resource "aws_subnet" "pub-subnet-az1" {
 
 # Public Subnet AZ 2
 resource "aws_subnet" "pub-subnet-az2" {
-  vpc_id     = aws_vpc.dev-vpc.id
+  vpc_id     = aws_vpc.dynamic-app-vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-west-1b"
   map_public_ip_on_launch = true
@@ -41,7 +41,7 @@ resource "aws_subnet" "pub-subnet-az2" {
 
 # Private App Subnet AZ 1
 resource "aws_subnet" "priv-app-subnet-az1" {
-  vpc_id = aws_vpc.dev-vpc.id
+  vpc_id = aws_vpc.dynamic-app-vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-west-1a"
 
@@ -52,7 +52,7 @@ resource "aws_subnet" "priv-app-subnet-az1" {
 
 # Private App Subnet AZ 2
 resource "aws_subnet" "priv-app-subnet-az2" {
-  vpc_id = aws_vpc.dev-vpc.id
+  vpc_id = aws_vpc.dynamic-app-vpc.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "us-west-1b"
 
@@ -63,7 +63,7 @@ resource "aws_subnet" "priv-app-subnet-az2" {
 
 # Private Data Subnet AZ 1
 resource "aws_subnet" "priv-data-subnet-az1" {
-  vpc_id = aws_vpc.dev-vpc.id
+  vpc_id = aws_vpc.dynamic-app-vpc.id
   cidr_block = "10.0.4.0/24"
   availability_zone = "us-west-1a"
 
@@ -74,7 +74,7 @@ resource "aws_subnet" "priv-data-subnet-az1" {
 
 # Private Data Subnet AZ 2
 resource "aws_subnet" "priv-data-subnet-az2" {
-  vpc_id = aws_vpc.dev-vpc.id
+  vpc_id = aws_vpc.dynamic-app-vpc.id
   cidr_block = "10.0.5.0/24"
   availability_zone = "us-west-1b"
 
@@ -85,7 +85,7 @@ resource "aws_subnet" "priv-data-subnet-az2" {
 
 # Public Route Table
 resource "aws_route_table" "public-rt" {
-  vpc_id = aws_vpc.dev-vpc.id
+  vpc_id = aws_vpc.dynamic-app-vpc.id
 
   tags = {
     Name = "public route table"
@@ -96,7 +96,7 @@ resource "aws_route_table" "public-rt" {
 resource "aws_route" "public-route" {
   route_table_id = aws_route_table.public-rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.dev-igw.id
+  gateway_id = aws_internet_gateway.dynamic-app-vpc-igw.id
 }
 
 # Associating public route table with public subnet az 1
