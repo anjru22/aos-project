29d0d865fcf8c6524ec943fd29add98a91df5d10
Andrew Chu <andrewchu2298@gmail.com>
2024-04-02 00:10:51 -0700

    4-1-2024

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,20 +1,20 @@
 resource "aws_instance" "fleetcart-az1" {
-  instance_type = "t2.micro"
-  ami = "ami-005c5f98fa2c776eb"
-  availability_zone = "us-west-1a"
-  subnet_id = aws_subnet.priv-app-subnet-az1.id
-  vpc_security_group_ids = [ aws_security_group.web-server-sg.id ]
+  instance_type          = "t2.micro"
+  ami                    = "ami-005c5f98fa2c776eb"
+  availability_zone      = "us-west-1a"
+  subnet_id              = aws_subnet.priv-app-subnet-az1.id
+  vpc_security_group_ids = [aws_security_group.web-server-sg.id]
   tags = {
     Name = "FleetCart Web Server AZ 1"
   }
 }
 
 resource "aws_instance" "fleetcart-az2" {
-  instance_type = "t2.micro"
-  ami = "ami-005c5f98fa2c776eb"
-  availability_zone = "us-west-1b"
-  subnet_id = aws_subnet.priv-app-subnet-az2.id
-  vpc_security_group_ids = [ aws_security_group.web-server-sg.id ]
+  instance_type          = "t2.micro"
+  ami                    = "ami-005c5f98fa2c776eb"
+  availability_zone      = "us-west-1b"
+  subnet_id              = aws_subnet.priv-app-subnet-az2.id
+  vpc_security_group_ids = [aws_security_group.web-server-sg.id]
   tags = {
     Name = "FleetCart Web Server AZ 2"
   }
--- terraform/target-group.tf
+++ terraform/target-group.tf
@@ -3,14 +3,14 @@ resource "aws_lb_target_group" "elb-tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.dynamic-app-vpc.id
-    health_check {
+  health_check {
     path                = "/"
     protocol            = "HTTP"
-    matcher             = "200,301,302"  # Health check success status codes
-    timeout             = 120 #5 (Original Value)
-    interval            = 125 #30 (Original Value)
-    healthy_threshold   = 10 #5 (Original Value)
-    unhealthy_threshold = 10 #2 (Original Value)
+    matcher             = "200,301,302" # Health check success status codes
+    timeout             = 120           #5 (Original Value)
+    interval            = 125           #30 (Original Value)
+    healthy_threshold   = 10            #5 (Original Value)
+    unhealthy_threshold = 10            #2 (Original Value)
   }
 }
 
